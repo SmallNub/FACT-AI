1870b990baf0ddfbcc5ad8c35aa30a2dbfca7802
SakrIsmail <sakrismail265@gmail.com>
2026-01-09 13:49:47 +0100

    moral.sh

--- MORAL/moral.sh
+++ MORAL/moral.sh
@@ -10,9 +10,9 @@
 
 set -euo pipefail
 
-python3 main.py --fair_model moral --model gae --dataset credit --device cuda:1 --epochs 500
-python3 main.py --fair_model moral --model gae --dataset german --device cuda:1 --epochs 500
-python3 main.py --fair_model moral --model gae --dataset nba --device cuda:1 --epochs 500
-python3 main.py --fair_model moral --model gae --dataset facebook --device cuda:1 --epochs 500
-python3 main.py --fair_model moral --model gae --dataset pokec_n --device cuda:0 --epochs 500
-python3 main.py --fair_model moral --model gae --dataset pokec_z --device cuda:2 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset credit --device cuda:0 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset german --device cuda:0 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset nba --device cuda:0 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset facebook --device cuda:0 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset pokec_n --device cuda:0 --epochs 500
+srun python main.py --fair_model moral --model gae --dataset pokec_z --device cuda:0 --epochs 500
