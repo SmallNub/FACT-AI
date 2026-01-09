0729ef47ad4e1e285f21387b6ba6a0532686e5b9
SakrIsmail <sakrismail265@gmail.com>
2026-01-09 13:52:29 +0100

    moral script

--- MORAL/moral.sh
+++ MORAL/moral.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=gpt_train
-#SBATCH --output=gpt_train_%j.log
-#SBATCH --error=gpt_train_%j.err
-#SBATCH --time=2:00:00
-#SBATCH --partition=gpu_mig
+#SBATCH --job-name=moral_train
+#SBATCH --output=moral_train_%j.log
+#SBATCH --error=moral_train_%j.err
+#SBATCH --time=12:00:00
+#SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=9
 #SBATCH --ntasks=1
 
 set -euo pipefail
+
+module purge
+module load 2024
+module load Anaconda3/2024.06-1
+
+source activate FACT
 
 srun python main.py --fair_model moral --model gae --dataset credit --device cuda:0 --epochs 500
 srun python main.py --fair_model moral --model gae --dataset german --device cuda:0 --epochs 500
